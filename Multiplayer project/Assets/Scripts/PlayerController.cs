ae58d352c35d519b4ad5b727f824adcd32d848d0
William Ackemo <william.ackemo@hotmail.com>
2019-03-15 12:32:08 +0100

    Test
    Testing to push up code

---
+++ Multiplayer project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody2D theRB;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode attack;
+    public Transform groundCheckPoint;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        } else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        } else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+		
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+	}
+}
