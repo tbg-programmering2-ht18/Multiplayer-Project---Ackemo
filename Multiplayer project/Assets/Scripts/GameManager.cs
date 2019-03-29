6f16bbfb243e8d09a31defec987639e8eee61823
William Ackemo <william.ackemo@hotmail.com>
2019-03-29 13:29:46 +0100

    Fixed player 2 and more
    Started to create the point system

---
+++ Multiplayer project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public int P1hp;
+    public int P2hp;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(P1hp <= 0)
+        {
+            player1.SetActive(false);
+        }
+
+        if (P2hp <= 0)
+        {
+            player2.SetActive(false);
+        }
+    }
+}
