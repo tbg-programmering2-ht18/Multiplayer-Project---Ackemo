d873ebcf398da42abf8d8ac9a8e0e3218b290f38
William Ackemo <william.ackemo@hotmail.com>
2019-04-11 09:51:52 +0200

    Added Content
    added a main menu and a controler menu, and a navigation way to them!

---
+++ Multiplayer project/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public string mainGameScene;
+    public string controlsScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(mainGameScene);
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(controlsScene);
+    }
+
+	public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
